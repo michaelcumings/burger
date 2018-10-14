c006a9f18f064945eeb1d7204710d7ba9c130ffe
Michael Cumings <michael.cumings@gmail.com>
2018-10-13 22:58:16 -0500

    structure

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Bleu', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale With Cheese', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', false);
